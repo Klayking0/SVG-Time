cba53a6edde4462ede769da1c2b6dba701c92cea
Klayking0 <105227096+Klayking0@users.noreply.github.com>
2022-05-12 01:45:05 +0100

    Initial commit

---
+++ js/draw.js
@@ -0,0 +1,20 @@
+
+const multiStr = `
+    Hey there!
+    How are you?
+    Do you 
+    have 
+    time
+    for a
+    quick 
+    call?
+`;
+
+const str = "Hello, world!";
+
+//const watchContainer = document.getElementById('watchContainer');
+//watchContainer.innerHTML = str;
+
+//document.getElementById('watchContainer').innerHTML += str;
+
+//https://www.freecodecamp.org/news/svg-javascript-tutorial/?msclkid=1949c10dd06e11ec9ebeb9f4e5aea11c
